c5213042c42dfe5c6c65109470197fda4a18547d
Berk DÜNDAR <berkodundar@gmail.com>
2021-12-17 19:44:53 +0300

    userpanelChanges

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -12,6 +12,7 @@ export default function Navbar() {
     return (
         <>
         <header>
+        <Userpanel/>
             <nav className={styles.nav}>
             <div className={styles.logo}>
             <Link href="/" passHref><Image src={Logo} alt="logo"/></Link>
@@ -21,7 +22,6 @@ export default function Navbar() {
                 <li><Link href="MyList">Listem</Link></li>
                 <li><Link href="Program">Günün programı</Link></li>
                 <li><Link href="Kayit">Kayıt ol</Link></li>
-                <li><Userpanel/></li>
                 
             </ul>
             </nav>
